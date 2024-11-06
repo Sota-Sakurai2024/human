fe210f14f8df2c4ef993a1372f826e43f23a9c16
Sota Sakurai <qqzp47vd@gmail.com>
2024-11-06 13:50:59 +0900

    挿入ソート関数作成

--- 20241030/InsertSort.cpp
+++ 20241030/InsertSort.cpp
@@ -12,6 +12,24 @@ void swapChar(char *lhs, char *rhs)
     *lhs = *rhs;
     *rhs = temp;
 }
+//挿入ソートの関数
+void InsertSort(char character[], int size)
+{
+    int index;
+    
+    //一つ目は整列済みなので配列の次の値からループさせる
+    for (int i = 1; i < size; i++)
+    {
+        index = i;
+        //整列されていたらここは実行されない
+        while ((index > 0) && (character[index - 1] > character[index]))
+        {
+            //値を入れ替える
+            swapChar(&character[index - 1], &character[index]);
+            index--;
+        }
+    }
+}
 int main()
 {
     char human[] = {'H', 'U', 'M', 'A', 'N', 'A', 'C', 'A', 'D', 'M', 'Y'};
