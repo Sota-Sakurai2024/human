69bbaab6811a39db9e559ee573cd2b2400bc8293
Sota Sakurai <qqzp47vd@gmail.com>
2024-11-06 13:50:07 +0900

    ポインタ関数

--- 20241030/InsertSort.cpp
+++ 20241030/InsertSort.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+//値を入れ替える関数
+void swapChar(char *lhs, char *rhs)
+{
+    //値を一時保存する変数
+    char temp;
+    
+    temp = *lhs;
+    *lhs = *rhs;
+    *rhs = temp;
+}
 int main()
 {
     char human[] = {'H', 'U', 'M', 'A', 'N', 'A', 'C', 'A', 'D', 'M', 'Y'};
