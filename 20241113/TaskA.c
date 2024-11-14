e0aebd334a3d6a48d2a817b45d6de9905ccb9aa6
Sota Sakurai <qqzp47vd@gmail.com>
2024-11-14 14:41:54 +0900

    data配列をシャッフルする

--- 20241113/TaskA.c
+++ 20241113/TaskA.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define ARRAY_SIZE  11
+
+void shuffle(int array[], int size)
+{
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        int j = rand() % size;
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
+
 int main()
 {
     int data[ARRAY_SIZE];
@@ -8,5 +24,13 @@ int main()
         data[i] = i;
         printf("%d", data[i]);
     }
+
+    shuffle(data, ARRAY_SIZE);
+
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        printf("%d回目:%d\n", i,data[i]);
+    }
+
     return 0;
 }
