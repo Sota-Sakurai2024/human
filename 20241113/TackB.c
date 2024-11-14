2c35f7644975fb5eba07c6f2e841752baada7a46
Sota Sakurai <qqzp47vd@gmail.com>
2024-11-14 18:20:53 +0900

    AscendingOrder関数作成

--- 20241113/TackB.c
+++ 20241113/TackB.c
@@ -4,6 +4,27 @@
 
 #define ARRAY_SIZE  100
 
+void AscendingOrder(int array[], int size)
+{
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        for (int j = ARRAY_SIZE - 1; j >= i + 1; j--)
+        {
+            int valueA = array[i];
+            int valueB = array[j];
+            
+            if (valueA > valueB)
+            {
+                int max = valueA;
+                int min = valueB;
+
+                array[i] = min;
+                array[j] = max;
+            }
+        }
+    }
+}
+
 int main()
 {
     int data[ARRAY_SIZE];
