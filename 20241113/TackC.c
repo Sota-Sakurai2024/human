4ec84eb5c495731a31484cbf7055b0f52a6ff214
Sota Sakurai <qqzp47vd@gmail.com>
2024-11-14 18:51:04 +0900

    乱数が入っている配列をソートする

--- 20241113/TackC.c
+++ 20241113/TackC.c
@@ -4,6 +4,29 @@
 
 #define ARRAY_SIZE  100
 
+//値を入れ替える関数
+void AscendingOrder(int array[], int size)
+{
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        for (int j = ARRAY_SIZE - 1; j >= i + 1; j--)
+        {
+            //それぞれに配列の値を格納
+            int valueA = array[i];
+            int valueB = array[j];
+            
+            //一つ目の値が二つ目の値より大きければ値を入れ替える
+            if (valueA > valueB)
+            {
+                int max = valueA;
+                int min = valueB;
+
+                array[i] = min;
+                array[j] = max;
+            }
+        }
+    }
+}
 int main()
 {
     int data[ARRAY_SIZE];
@@ -13,5 +36,7 @@ int main()
         data[i] = rand() % 100 + 1;
         printf("%d番目:%d\n", i, data[i]);
     }
+
+    AscendingOrder(data, ARRAY_SIZE);
     return 0;
 }
