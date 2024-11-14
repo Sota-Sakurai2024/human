6285d10352910de607e79a5d91140ee34ac6f3bb
Sota Sakurai <qqzp47vd@gmail.com>
2024-11-14 17:19:31 +0900

    乱数を格納

---
+++ 20241113/TackB.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE  100
+
+int main()
+{
+    int data[ARRAY_SIZE];
+
+    srand(time(NULL));
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        data[i] = rand();
+        printf("%d番目:%d\n", i, data[i]);
+    }
+    return 0;
+}
